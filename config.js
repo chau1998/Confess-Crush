9de7474783f338832e2b513e15b7ebb537fd2c0a
chau1998 <91309616+chau1998@users.noreply.github.com>
2021-09-24 14:22:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay, mình mình điện thoại nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100009088452946/
+    messLink: 'https://www.facebook.com/messages/t/100009088452946/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100009088452946/
 }
